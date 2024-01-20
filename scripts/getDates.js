f905ef66425533e257b9cc4df2376053759fac18
abrahamnyuiadzi <abrahamnyuiadzi@gmail.com>
2024-01-20 09:52:55 +0000

    commit one

--- scripts/getDates.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('#lastModified').textContent = "Last Modification: "+document.lastModified;
-
-const rightNow = new Date();
-document.querySelector('#year').textContent = rightNow.getFullYear();
