0a04bd88bc92da7a481336b86e534fbaee26f7f4
abrahamnyuiadzi <abrahamnyuiadzi@gmail.com>
2024-01-20 17:52:23 +0000

    hamburger

--- week1/scripts/getDates.js
+++ week1/scripts/getDates.js
@@ -2,3 +2,29 @@ document.querySelector('#lastModified').textContent = "Last Modification: "+docu
 
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear();
+
+const hamburgerElement = document.querySelector('#mybutton')
+const navElement =document.querySelector('.menulinks')
+
+hamburgerElement.addEventListener('click',()=>{
+    navElement.classList.toggle('open');
+   hamburgerElement.classList.toggle('open');
+
+})
+
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
+
