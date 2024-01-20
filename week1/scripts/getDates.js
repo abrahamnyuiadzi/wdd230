391e19c328b535ba90d298a83538184aeabf624f
abrahamnyuiadzi <abrahamnyuiadzi@gmail.com>
2024-01-20 09:52:39 +0000

    week1

---
+++ week1/scripts/getDates.js
@@ -0,0 +1,4 @@
+document.querySelector('#lastModified').textContent = "Last Modification: "+document.lastModified;
+
+const rightNow = new Date();
+document.querySelector('#year').textContent = rightNow.getFullYear();
