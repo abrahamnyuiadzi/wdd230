8d8733fd70ef6a4d299d89d023314405b9f1a089
abrahamnyuiadzi <abrahamnyuiadzi@gmail.com>
2024-01-20 09:39:49 +0000

    commit

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,17 +1,4 @@
-alert(document.lastModified);
-// returns: Tuesday, December 16, 2017 11:09:42
-let oLastModif = new Date(document.lastModified);
-let nLastModif = Date.parse(document.lastModified);
-// Match 'timestamp' in 'last_modif=timestamp'
-// e.g. '1687964614822' in 'last_modif=1687964614822'
-const pattern = /last_modif\s*=\s*([^;]*)/;
+document.querySelector('#lastModified').textContent = "Last Modification: "+document.lastModified;
 
-if (
-  Date.parse(document.lastModified) >
-  (parseFloat(document.cookie.match(pattern)?.[1]) || 0)
-) {
-  document.cookie = `last_modif=${Date.now()}; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=${
-    location.pathname
-  }`;
-  alert("This page has changed!");
-}
+const rightNow = new Date();
+document.querySelector('#year').textContent = rightNow.getFullYear();
